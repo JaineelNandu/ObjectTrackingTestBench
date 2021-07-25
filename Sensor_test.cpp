ea3933d72ea2a7cf1c7f33b99647e9769e8885f8
Jaineel <nandujaineel@vt.edu>
2021-07-25 16:14:52 -0400

    Tests Lists of Obstacles

--- Sensor_test.cpp
+++ Sensor_test.cpp
@@ -29,9 +29,24 @@ TEST(SensorFOVTests, SensorFOVNoisyPassedTest) {
     s1.passThrough(list_active, points);
     vector<int> passed_active = s1.getActivePassed();
     vector<vector<double> > passed_points = s1.getPassed();
-    printVector(passed_points);
     ASSERT_TRUE(areEqualVectors(passed_active, truth_active));
     EXPECT_TRUE(areEqualVectors(passed_points, truth_passed, 1.5)); // Tolerance is 3 std of maximum std i.e. 0.5
+}
+
+TEST(ObstacleListTest, GetPassedObstacleList) {
+    Sensor s1({270, 40, 3, 150}, {0.01, 0.05, 0.5}, SensorType::Lidar2);
+    vector<int> list_active = {1, 2, 3, 4, 5, 6};
+    vector<vector<double> > points = {{20.5, 11, 12}, {4, 5, 1}, {4, 5, 10}, {1, 2, 36}, {143, 2, 1}, {-39, -60, 1}};
+    s1.passThrough(list_active, points);
+    vector<int> passed_active = s1.getActivePassed();
+    vector<vector<double> > passed_points = s1.getPassed();
+    vector<Obstacle> passed_obstacles = s1.pointToObstacle();
+    for(int i = 0; i < passed_active.size(); i++) {
+        Obstacle o1;
+        o1.setPosition(passed_points[i]);
+        o1.setSensorType(SensorType::Lidar2);
+        ASSERT_EQ(o1, passed_obstacles[i]);
+    }
 }
 
 int main(int argc, char **argv)
